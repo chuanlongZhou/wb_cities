65f18e51ce8bd492f78066f80148ba673a1da99c
Defve <chuanlong.zhou@lsce.ipsl.fr>
2022-08-03 12:27:09 +0200

    1

--- OSM/__init__.py
+++ OSM/__init__.py
@@ -4,3 +4,4 @@ from .OSM import OSM
 # https://wiki.openstreetmap.org/wiki/Buildings
 # https://wiki.openstreetmap.org/wiki/Key:amenity
 # https://osmbuildings.org/documentation/data/
+# https://overpass-turbo.eu/
